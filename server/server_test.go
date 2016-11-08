5b352d1e525fea01e1de0c7c2221780c5814cac4
Venkata Dinesh Yeduguru <dyeduguru@palantir.com>
2016-11-07 19:09:26 -0800

    create server pkg

---
+++ server/server_test.go
@@ -0,0 +1,11 @@
+package server_test
+
+import (
+	"testing"
+	"github.com/coreos/etcd/Godeps/_workspace/src/github.com/stretchr/testify/assert"
+)
+
+func TestServer(t *testing.T) {
+
+	assert.Equal(t, "input", actual, "The string returned does not match expected value")
+}
